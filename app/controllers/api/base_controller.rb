80e9a16a82c028e142b7f6d009a6ba02aed7f51b
BorisSIUS <balfooldi@yahoo.fr>
2021-11-25 15:19:18 +0100

    Create ApiJson service

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,8 +3,10 @@ class Api::BaseController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
-  private
+  include ApiJson
   
+  private
+
   def not_found
     render json: {
       'errors': [
