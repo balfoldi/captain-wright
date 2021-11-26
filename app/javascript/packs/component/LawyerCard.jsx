377d17af61e97834f435f0133574b5717c5e74f6
BorisSIUS <balfooldi@yahoo.fr>
2021-11-26 11:30:29 +0100

    Add animations to lawyer card

--- app/javascript/packs/component/LawyerCard.jsx
+++ app/javascript/packs/component/LawyerCard.jsx
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card'
 import mia from '../../images/avatars/mia/miaIdle.gif'
 import pheonix from '../../images/avatars/pheonix/pheonixIdle.gif'
 
+import { motion } from "framer-motion"
 
 const LawyerCard = ({ lawyer }) => {
   const gifs = {
@@ -12,16 +13,21 @@ const LawyerCard = ({ lawyer }) => {
 
   const { avatar, full_name, speechcraft, credibility } = lawyer
   return (
-    <Card className="m-2 shadow" style={{ width: "16rem" }}>
-      <Card.Img src={gifs[avatar]}/>
-      <Card.Body>
-        <Card.Title>{full_name}</Card.Title>
-        <ul>
-          <li>Speechcraft: {speechcraft}</li>
-          <li>Credibility: {credibility}</li>
-        </ul>
-      </Card.Body>
-    </Card>
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <Card className="m-2 shadow" style={{ width: "16rem" }}>
+        <Card.Img src={gifs[avatar]} />
+        <Card.Body>
+          <Card.Title>{full_name}</Card.Title>
+          <ul>
+            <li>Speechcraft: {speechcraft}</li>
+            <li>Credibility: {credibility}</li>
+          </ul>
+        </Card.Body>
+      </Card>
+    </motion.div>
   )
 }
 
