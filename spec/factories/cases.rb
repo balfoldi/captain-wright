fdf504e640df05a63d9a48c8faa2f6dd2997f8a8
BorisSIUS <balfooldi@yahoo.fr>
2021-11-27 10:21:00 +0100

    Create case requests tests

--- spec/factories/cases.rb
+++ spec/factories/cases.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :case do
-    
+    winner { FactoryBot.create(:lawyer) }
+    looser { FactoryBot.create(:lawyer) }
   end
 end
--- spec/requests/api/cases_request_spec.rb
+++ spec/requests/api/cases_request_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Cases", type: :request do
+  subject { JSON.parse(response.body, {:symbolize_names => true}) }
 
   describe "GET /index" do
-    it "returns http success" do
-      get "/api/cases/index"
-      expect(response).to have_http_status(:success)
-    end
+    let!(:cases) { [FactoryBot.create_list(:case, 4)] }
+
+    before { get api_cases_path }
+
+    it { is_expected.to have_attributes(size: 4) }
   end
 
   describe "GET /create" do
-    it "returns http success" do
-      get "/api/cases/create"
-      expect(response).to have_http_status(:success)
-    end
+    let(:looser) { FactoryBot.create(:lawyer) }
+    let(:winner) { FactoryBot.create(:lawyer) }
+    let(:params) { { case: { winner_id: winner.id, looser_id: looser.id } } }
+
+    before { post api_cases_path, params: params }
+
+    it { is_expected.to include(params[:case]) }
   end
 
 end
