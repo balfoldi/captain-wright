1ac84570e271faa6ff3f377383ad608aa7d88f46
BorisSIUS <balfooldi@yahoo.fr>
2021-11-27 11:28:24 +0100

    Fix features lawyers ci

---
+++ app/javascript/packs/component/Avatars.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import pheonixIdle from '../../images/avatars/pheonix/pheonixIdle.gif'
+import pheonixNo from '../../images/avatars/pheonix/pheonixNo.gif'
+import pheonixObjected from '../../images/avatars/pheonix/pheonixObjected.gif'
+import pheonixObjection from '../../images/avatars/pheonix/pheonixObjection.gif'
+import pheonixSpeech from '../../images/avatars/pheonix/pheonixSpeech.gif'
+
+import miaIdle from '../../images/avatars/mia/miaIdle.gif'
+import miaNo from '../../images/avatars/mia/miaNo.gif'
+import miaObjected from '../../images/avatars/mia/miaObjected.gif'
+import miaObjection from '../../images/avatars/mia/miaObjection.gif'
+import miaSpeech from '../../images/avatars/mia/miaSpeech.gif'
+
+
+export const Pheonix = ({state, playerName}) => {
+
+  const renderGifs = {
+    idle: pheonixIdle,
+    defend: pheonixNo,
+    defeated: pheonixObjected,
+    object: pheonixObjection,
+    speak: pheonixSpeech,
+  }
+
+  return (
+    <div>
+      <img src={renderGifs[state]} className={playerName == "right" ? "invert" : ""}/>
+    </div>
+  )
+}
+
+export const Mia = ({state, playerName}) => {
+
+  const renderGifs = {
+    idle: miaIdle,
+    defend: miaNo,
+    defeated: miaObjected,
+    object: miaObjection,
+    speak: miaSpeech,
+  }
+
+  return (
+    <div>
+      <img src={renderGifs[state]} className={playerName == "right" ? "invert" : ""}/>
+    </div>
+  )
+}
